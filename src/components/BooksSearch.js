0a3b2ac5e1bbfff304d9cbfb2517c59a97e5526d
diogoku <diogomnribeiro@gmail.com>
2020-08-30 19:08:33 +0100

    search book with filter

--- src/components/BooksSearch.js
+++ src/components/BooksSearch.js
@@ -4,32 +4,37 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getBooks } from "../actions/booksActionsCreator";
 
+// REACT-HOOK-FORMS
+import { useForm } from "react-hook-form";
+
 // AXIOS
 import axios from "axios";
 
 function BooksSearch() {
-  const [bookFilter, setBookFilter] = useState("search+terms");
   const [errorMessage, setErrorMessage] = useState("");
+
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchBooksData = async () => {
-      const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=search+terms`
-      );
+  const fetchBooksData = async (data) => {
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${data.bookTittle}`
+    );
 
-      if (response.status == 200) {
-        dispatch(getBooks(response.data.items));
-        setErrorMessage("");
-      } else setErrorMessage("The google books API is currently unavailable");
-    };
-    fetchBooksData();
-  }, []);
+    if (response.status == 200) {
+      dispatch(getBooks(response.data.items));
+      setErrorMessage("");
+    } else setErrorMessage("The google books API is currently unavailable");
+  };
+
+  const { register, handleSubmit, errors } = useForm();
 
   return (
     <div>
       {errorMessage === "" ? null : <p>{errorMessage}</p>}
-      <form className="search-book-form">
+      <form
+        className="search-book-form"
+        onSubmit={handleSubmit(fetchBooksData)}
+      >
         <h3>Search Book</h3>
         <label htmlFor="book-tittle" className="search-book-label">
           By Tittle
@@ -37,9 +42,11 @@ function BooksSearch() {
         <input
           type="text"
           id="book-tittle"
-          name="bookTitle"
+          name="bookTittle"
           className="search-book-input"
+          ref={register({ required: true })}
         />
+        {errors.bookTittle && <span>This field is required *</span>}
         <input type="submit" className="search-books-submit" value="Search" />
       </form>
     </div>
