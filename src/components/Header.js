fa7f9974469e077a02aa2b68ce75ba8b0e6c61e0
diogoku <diogomnribeiro@gmail.com>
2020-08-30 15:55:33 +0100

    css color variables change and header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 
+// CSS
+import "../css/header.css";
+
 // FONT AWESOME ICONS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
-    <div className="">
-      <h1>
+    <div className="header-section">
+      <h2>
         Google Books API <FontAwesomeIcon icon="book" />
-      </h1>
+      </h2>
     </div>
   );
 }
