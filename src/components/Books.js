fc37fd584ecdc8f4390568f4ebeb6043788012d9
diogoku <diogomnribeiro@gmail.com>
2020-08-30 16:49:25 +0100

    add @babel/plugin-transform-runtime to fetch data async and getch book data from google books API

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// AXIOS
+import axios from "axios";
 
 function Books() {
+  const [booksData, setBooksData] = useState();
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=search+terms`
+      );
+      console.log(response);
+      setBooksData(response);
+    };
+
+    fetchBooks();
+  }, []);
+
   return <div>Books Component</div>;
 }
 
