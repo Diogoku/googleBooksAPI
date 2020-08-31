02e02be6b9ae300573ef63234066e029544956ea
diogoku <diogomnribeiro@gmail.com>
2020-08-31 17:47:11 +0100

    working on displaying detail book data

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -13,10 +13,33 @@ function BookDetail() {
   const { booksData } = useSelector((state) => state.booksReducer);
 
   const detailBookData = booksData.filter((book) => book.id == bookId)[0];
-
+  console.log(detailBookData, "detalhado");
   return (
-    <div>
-      Book Card Detail {bookId}
+    <div className="detail-book-section">
+      <div className="book-main-details">
+        <div>
+          <img src={detailBookData.volumeInfo.imageLinks.thumbnail} alt="" />
+        </div>
+        <div className="book-main-details-extension">
+          <p className="book-title">
+            {detailBookData.volumeInfo.title}:{" "}
+            {detailBookData.volumeInfo.subtitle}
+          </p>
+          {detailBookData.volumeInfo.authors
+            ? detailBookData.volumeInfo.authors.map((author) => {
+                return <p className="book-author">{author}</p>;
+              })
+            : null}
+          <p>{detailBookData.volumeInfo.publisher}</p>
+          <p>Published on: {detailBookData.volumeInfo.publishedDate}</p>
+          <p>{detailBookData.volumeInfo.pageCount} Pages</p>
+          <p>{detailBookData.volumeInfo.averageRating}</p>
+        </div>
+      </div>
+      <div className="book-description">
+        <p>{detailBookData.volumeInfo.description}</p>
+      </div>
+
       <button onClick={history.goBack}>Go Back</button>
     </div>
   );
